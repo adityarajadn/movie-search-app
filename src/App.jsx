195c9b34d013e4837c97e9f4c6bea22ddeeb8632
rawrjaaa” <162077143+rawrjaaa@users.noreply.github.com>
2026-01-14 15:35:25 +0700

    fetch API

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,39 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [movies, setMovies] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)  
 
+  const fetchMovies = async () => {
+    try {
+      setLoading(true)
+      setError(null)
+
+      const response = await fetch(
+        'http://www.omdbapi.com/?i=tt3896198&apikey=9aa57373'
+      )
+      const data = await response.json()
+
+      if (data.Response === 'False') {
+        throw new Error(data.Error)
+      }
+
+      setMovies(data.Search)
+    } catch (err) {
+      setError(err.message)
+    } finally {
+      setLoading(false)
+    }
+
+    useEffect(() => {
+      fetchMovies()
+    }, [])
+    
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
